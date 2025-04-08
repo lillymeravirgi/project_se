1261dbd71200985a1467e13bbb37cce66d4e3a44
kira <k.frielingsdorf@student.maastrichtuniversity.nl>
2025-04-08 16:18:30 +0200

    aligned the FileOutputSTrategy file with Google Java Style Guide

--- src/main/java/com/cardio_generator/outputs/fileOutputStrategy.java
+++ src/main/java/com/cardio_generator/outputs/fileOutputStrategy.java
@@ -7,43 +7,49 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.concurrent.ConcurrentHashMap;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.concurrent.Executors;
+//deleted recurrend imports and imports that arent used
 
 //changed fileOutputStrategy to FileOutputStrategy as classes need to be written in UpperCamelCase
-//see google java style guide 5.2.2 
 public class FileOutputStrategy implements OutputStrategy {
 
-    private String BaseDirectory;
+    //changed baseDirectory to lowerCamelCase
+    private String baseDirectory;
 
-    public final ConcurrentHashMap<String, String> file_map = new ConcurrentHashMap<>();
+    //changed fileMap to lowerCamelCase
+    public final ConcurrentHashMap<String, String> fileMap = new ConcurrentHashMap<>();
 
     public FileOutputStrategy(String baseDirectory) {
 
-        this.BaseDirectory = baseDirectory;
+        this.baseDirectory = baseDirectory;
     }
 
     @Override
+    /**
+     * @param patientId the ID of the patient 
+     * @param timestamp the time at which the measurement was taken, in milliseconds 
+     * @param label the type of data eg. ECG, Saturdation,... 
+     * @param data data value eg. 90.0%
+     * 
+     * @return the patient data is added to file
+     * 
+     */
     public void output(int patientId, long timestamp, String label, String data) {
         try {
             // Create the directory
-            Files.createDirectories(Paths.get(BaseDirectory));
+            Files.createDirectories(Paths.get(baseDirectory));
         } catch (IOException e) {
             System.err.println("Error creating base directory: " + e.getMessage());
             return;
         }
-        // Set the FilePath variable
-        String FilePath = file_map.computeIfAbsent(label, k -> Paths.get(BaseDirectory, label + ".txt").toString());
+        // Set the filePath variable
+        //changed fielPath to lowerCamelCase
+        String filePath = fileMap.computeIfAbsent(label, k -> Paths.get(baseDirectory, label + ".txt").toString());
 
         // Write the data to the file
-        try (PrintWriter out = new PrintWriter(
-                Files.newBufferedWriter(Paths.get(FilePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
+        try (PrintWriter out = new PrintWriter(Files.newBufferedWriter(Paths.get(filePath), StandardOpenOption.CREATE, StandardOpenOption.APPEND))) {
             out.printf("Patient ID: %d, Timestamp: %d, Label: %s, Data: %s%n", patientId, timestamp, label, data);
         } catch (Exception e) {
-            System.err.println("Error writing to file " + FilePath + ": " + e.getMessage());
+            System.err.println("Error writing to file " + filePath + ": " + e.getMessage());
         }
     }
     
