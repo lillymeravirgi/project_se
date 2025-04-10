c6a70ba625253e6dea9e8be7949f9a57e4ad9062
kira <k.frielingsdorf@student.maastrichtuniversity.nl>
2025-04-10 08:53:37 +0200

    applied a change to commenting

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -3,12 +3,12 @@ package com.cardio_generator.generators;
 import com.cardio_generator.outputs.OutputStrategy;
 
 /**
- * Inteface for several different types of data generation
+ * Inteface for several different types of data generation.
+ * It outputs/inserts it in the chosen output strategy.
  * 
  * @param patientId specifies the patient
  * @param outputStrategy an element of the type outputstrategy specifies the way in which the data is stored/displayed
  * 
- * @return Randomly synthesises health data and outputs/inserts it in the chosen way.
  */
 public interface PatientDataGenerator {
     void generate(int patientId, OutputStrategy outputStrategy);
