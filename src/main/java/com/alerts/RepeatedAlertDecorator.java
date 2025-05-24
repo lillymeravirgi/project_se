d5f967141e5b28b9fb8a5f96294d3fde5c23f78b
kira <k.frielingsdorf@student.maastrichtuniversity.nl>
2025-05-24 11:17:05 +0200

    added repeated checking to repeated alert decorator

--- src/main/java/com/alerts/RepeatedAlertDecorator.java
+++ src/main/java/com/alerts/RepeatedAlertDecorator.java
@@ -1,24 +1,46 @@
 package com.alerts;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class RepeatedAlertDecorator extends AlertDecorator{
 
-    private int repeatCount;
-    private long repeatIntervalMs;
+    private int repetitions;
+    private long repeatIntervalSec;
+
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private int runCount = 0;
     
 
-    public RepeatedAlertDecorator(Alert alert, String repetitionCondition) {
+    public RepeatedAlertDecorator(Alert alert, int repetitions,long repeatIntervalSec) {
         super(alert);
 
-        this.repeatCount = repeatCount;
-        this.repeatIntervalMs = repeatIntervalMs;
+        this.repetitions = repetitions;
+        this.repeatIntervalSec = repeatIntervalSec;
 
+        startRepeatedChecking();
+    }
 
+    private void startRepeatedChecking() {
+        scheduler.scheduleAtFixedRate(() -> {
+            runCount++;
+            System.out.println("Repeated check #" + runCount + " for patient " + getPatientId() +
+                               " | Condition: " + getCondition() +
+                               " | Timestamp: " + System.currentTimeMillis());
+
+            if (runCount >= repetitions) {
+                System.out.println("Max repeats reached. Stopping checks.");
+                scheduler.shutdown();
+            }
+
+        }, 0, repeatIntervalSec, TimeUnit.SECONDS);
     }
     
     @Override
     public String getAllDetails() {
         
         return baseAlert.getAllDetails() +
-               " | Repeated " + repeatCount + "x every " + repeatIntervalMs + "ms";
+               " | Repeated " + repetitions + " times every " + repeatIntervalSec + " Seconds";
     }
 }
